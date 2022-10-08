d854fb0237731eb840871d214a2dba435537e933
Xintong Mi <xintongmi@Xintongs-MacBook-Pro.local>
2022-10-08 12:15:14 -0700

    refactor API implementation to get rid of Spring Data REST

--- spring-boot-accounting-book/src/main/java/com/xintongthecoder/accountingbook/dao/AccountBookRepository.java
+++ spring-boot-accounting-book/src/main/java/com/xintongthecoder/accountingbook/dao/AccountBookRepository.java
@@ -1,5 +1,7 @@
 package com.xintongthecoder.accountingbook.dao;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
@@ -7,5 +9,8 @@ import com.xintongthecoder.accountingbook.entity.AccountBook;
 
 @CrossOrigin("http://localhost:4200")
 public interface AccountBookRepository extends JpaRepository<AccountBook, Long> {
-    
+
+    Page<AccountBook> findById(Long id, Pageable pageable);
+
+    Page<AccountBook> findAll(Pageable pageable);
 }
