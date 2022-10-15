b75afae4a4fe8e82b3224e7ff1104486f7213326
Xintong Mi <xintongmi@Xintongs-MacBook-Pro.local>
2022-10-14 20:13:22 -0700

    secure the frontend by adding access token to request header

---
+++ angular-accounting-book/src/app/services/auth-interceptor.service.ts
@@ -0,0 +1,47 @@
+import {
+  HttpEvent,
+  HttpHandler,
+  HttpInterceptor,
+  HttpRequest,
+} from '@angular/common/http';
+import { Inject, Injectable } from '@angular/core';
+import { OKTA_AUTH } from '@okta/okta-angular';
+import { OktaAuth } from '@okta/okta-auth-js';
+import { from, lastValueFrom, Observable } from 'rxjs';
+import { getBackendBaseUrl } from '../common/utils';
+import { ApiEntitySegments } from '../data-types';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthInterceptorService implements HttpInterceptor {
+  constructor(@Inject(OKTA_AUTH) private readonly oktaAuth: OktaAuth) {}
+
+  intercept(
+    request: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    return from(this.handleAccess(request, next));
+  }
+  private handleAccess(
+    request: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    // Add an access token for secured endpoints
+    const securedEndpoints = [
+      `${getBackendBaseUrl()}/${ApiEntitySegments.ACCOUNTS}`,
+    ];
+    if (securedEndpoints.some((url) => request.urlWithParams.includes(url))) {
+      // Get access token
+      const accessToken = this.oktaAuth.getAccessToken();
+      // Clone the request and add new header with access token
+      request = request.clone({
+        setHeaders: {
+          Authorization: 'Bearer ' + accessToken,
+        },
+      });
+    }
+
+    return from(lastValueFrom(next.handle(request)));
+  }
+}
