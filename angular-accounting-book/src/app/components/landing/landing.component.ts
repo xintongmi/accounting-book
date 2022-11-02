e1775604190be8a67c74053eabf766b3153d5aac
Xintong Mi <xintongmi@Xintongs-MacBook-Pro.local>
2022-11-01 21:12:36 -0700

    style landing page with carousel

--- angular-accounting-book/src/app/components/landing/landing.component.ts
+++ angular-accounting-book/src/app/components/landing/landing.component.ts
@@ -8,9 +8,26 @@ import { AuthTransaction, OktaAuth } from '@okta/okta-auth-js';
   styleUrls: ['./landing.component.scss'],
 })
 export class LandingComponent implements OnInit {
+  slides: any[] = new Array(3).fill({
+    id: -1,
+    src: '',
+    title: '',
+    subtitle: '',
+  });
+
   constructor(@Inject(OKTA_AUTH) private oktaAuth: OktaAuth) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.slides[0] = {
+      src: 'https://mdbcdn.b-cdn.net/img/new/slides/041.webp',
+    };
+    this.slides[1] = {
+      src: 'https://mdbcdn.b-cdn.net/img/new/slides/042.webp',
+    };
+    this.slides[2] = {
+      src: 'https://mdbcdn.b-cdn.net/img/new/slides/043.webp',
+    };
+  }
 
   signinWithTrialAccount() {
     const username = 'accbooktrial@gmail.com';
