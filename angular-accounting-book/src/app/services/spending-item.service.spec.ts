1e645ecc054dbd5294e10d9b8d6a5d4c7bbddb85
Xintong Mi <xintongmi@Xintongs-MacBook-Pro.local>
2022-11-03 16:37:28 -0700

    write test for addItem method of SpendingItemService

--- angular-accounting-book/src/app/services/spending-item.service.spec.ts
+++ angular-accounting-book/src/app/services/spending-item.service.spec.ts
@@ -1,24 +1,32 @@
 import { HttpClient } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
 import { of } from 'rxjs';
+import { SpendingItem } from '../data-types';
 import { AccountService } from './account.service';
 
 import { SpendingItemService } from './spending-item.service';
 
 fdescribe('SpendingItemService', () => {
   let service: SpendingItemService;
+  let mockAccountService: any;
+  let mockHttpClient: any;
 
   beforeEach(() => {
-    const mockHttpClient = jasmine.createSpyObj('HttpClient', [
+    mockHttpClient = jasmine.createSpyObj('HttpClient', [
       'get',
       'put',
       'post',
       'delete',
     ]);
-    const mockAccountService = jasmine.createSpyObj('AccountService', [
+    mockAccountService = jasmine.createSpyObj('AccountService', [
       'getAccountBaseUrl$',
     ]);
+
     mockAccountService.getAccountBaseUrl$.and.returnValue(of('baseUrl'));
+    mockHttpClient.get.and.returnValue(of(''));
+    mockHttpClient.put.and.returnValue(of(''));
+    mockHttpClient.post.and.returnValue(of(''));
+    mockHttpClient.delete.and.returnValue(of(''));
 
     TestBed.configureTestingModule({
       providers: [
@@ -42,6 +50,29 @@ fdescribe('SpendingItemService', () => {
     const result$ = service.getUrl({ itemId: 1 });
     result$.subscribe((v) => {
       expect(v).toEqual('baseUrl/items/1');
+      done();
+    });
+  });
+
+  // it('#getSpendingItemList should return correct spendingItemResponse', (done: DoneFn) => {});
+
+  it('#addItem should return correct spendingItem', (done: DoneFn) => {
+    const date = new Date();
+    const newItem: SpendingItem = {
+      id: 0,
+      bookId: 1,
+      category: 'household',
+      description: 'desc',
+      amount: 2,
+      date,
+      merchant: 'mer',
+    };
+    const result$ = service.addItem(newItem);
+    result$.subscribe((v) => {
+      expect(mockHttpClient.post).toHaveBeenCalledWith(
+        'baseUrl/books/1/items',
+        newItem
+      );
       done();
     });
   });
