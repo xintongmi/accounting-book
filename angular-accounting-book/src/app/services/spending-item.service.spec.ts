91a5f1a86a6d9a3bbd261edea48aae50aab963f7
Xintong Mi <xintongmi@Xintongs-MacBook-Pro.local>
2022-11-03 15:03:51 -0700

    write test for spendingItemService

--- angular-accounting-book/src/app/services/spending-item.service.spec.ts
+++ angular-accounting-book/src/app/services/spending-item.service.spec.ts
@@ -1,16 +1,48 @@
+import { HttpClient } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
+import { AccountService } from './account.service';
 
 import { SpendingItemService } from './spending-item.service';
 
-describe('SpendingItemService', () => {
+fdescribe('SpendingItemService', () => {
   let service: SpendingItemService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    const mockHttpClient = jasmine.createSpyObj('HttpClient', [
+      'get',
+      'put',
+      'post',
+      'delete',
+    ]);
+    const mockAccountService = jasmine.createSpyObj('AccountService', [
+      'getAccountBaseUrl$',
+    ]);
+    mockAccountService.getAccountBaseUrl$.and.returnValue(of('baseUrl'));
+
+    TestBed.configureTestingModule({
+      providers: [
+        { provide: HttpClient, useValue: mockHttpClient },
+        { provide: AccountService, useValue: mockAccountService },
+        SpendingItemService,
+      ],
+    });
     service = TestBed.inject(SpendingItemService);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  it('#getUrl with bookId should return correct url', (done: DoneFn) => {
+    const result$ = service.getUrl({ bookId: 1 });
+    result$.subscribe((v) => {
+      expect(v).toEqual('baseUrl/books/1/items');
+      done();
+    });
+  });
+
+  it('#getUrl with itemId should return correct url', (done: DoneFn) => {
+    const result$ = service.getUrl({ itemId: 1 });
+    result$.subscribe((v) => {
+      expect(v).toEqual('baseUrl/items/1');
+      done();
+    });
   });
 });
