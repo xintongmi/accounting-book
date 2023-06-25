888d98e222d36a04669fe6f81bd03b64bab1667c
XintongTheCoder <xintongmi@gmail.com>
2023-06-25 15:30:08 -0700

    change sub-domain

--- angular-accounting-book/src/app/config/app-config.ts
+++ angular-accounting-book/src/app/config/app-config.ts
@@ -2,7 +2,7 @@ export const APP_CONFIG = {
   oidc: {
     clientId: '0oa6u5tj1naCe3IdB5d7',
     issuer: 'https://dev-73315855.okta.com/oauth2/default',
-    redirectUri: 'https://www.xintongthecoder.com/login/callback',
+    redirectUri: 'https://xintongthecoder.com/login/callback',
     scopes: ['openid', 'profile', 'email'],
   },
 };
